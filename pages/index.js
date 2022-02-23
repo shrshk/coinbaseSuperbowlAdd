aa17faf7cd05e34791c85e18f9043427e94afa47
shirish <shirish101k@gmail.com>
2022-02-22 19:43:23 -0600

    made the superbowl add

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,88 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import QRCode from 'react-qr-code';
+import {useEffect, useRef} from "react";
+
+const colors = ["#AD1EC2", "#D36EA6", "#D0BDD1", "#83BD16", "#16BDB1"];
+
+const ballSize = 200;
+
+const isBoundary = (idx, dir, boundary) => {
+    return dir === 1 && idx + ballSize >= boundary || dir === -1 && idx <= 0;
+}
+
+const boxStyle = {
+    bgcolor: 'black',
+    height: '100vh',
+    '& > div': {
+        position: 'absolute'
+    }
+};
+
+const containerStyle = (x, y) =>  {
+   return {
+       top: y,
+       left: x
+   }
+}
 
 export default function Index() {
+  const container = useRef();
+
+  const [ball, setBall] = React.useState({
+      x: 0,
+      y: 0,
+      xDir: 1,
+      yDir: 1,
+  });
+
+  const [colorIdx, setColorIdx] = React.useState(0);
+
+  const { x, y, xDir, yDir } = ball;
+
+  const randColorIdx = () => {
+      return Math.floor(Math.random() * colors.length);
+  }
+
+  useEffect(() => {
+      if (!container.current)
+          return;
+
+      window.requestAnimationFrame(() => {
+          const width = container.current.offsetWidth;
+          const height = container.current.offsetHeight;
+
+          if (isBoundary(x, xDir, width)) {
+              setBall((currVal) => ({
+                  ...currVal,
+                  xDir: xDir * -1
+              }))
+              setColorIdx(randColorIdx());
+          } else if (isBoundary(y, yDir, height)) {
+              setBall((currVal) => ({
+                  ...currVal,
+                  yDir: yDir * -1
+              }))
+              setColorIdx(randColorIdx());
+          } else {
+              setBall((currVal) => ({
+                  ...currVal,
+                  x: x + xDir,
+                  y: y + yDir
+              }))
+          }
+      });
+
+  }, [colorIdx, x, y, xDir, yDir]);
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
+      <Box sx={boxStyle} ref={container}>
+          <Box style={containerStyle(x,y)}>
+              <QRCode
+                  value="https://computersandtacos.com"
+                  size={ballSize}
+                  bgColor={colors[colorIdx]}
+              />
+          </Box>
       </Box>
-    </Container>
   );
 }
